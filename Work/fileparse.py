bf23fc26e48c872429e523b7567769064c303aac
dedzr <salmancse.i77@gmail.com>
2024-10-25 16:35:47 +0600

    solved fileparse exercise

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,22 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import os
+import gzip
+
+
+f=gzip.open('Data/portfolio.csv.gz', 'rt')
+headers=next(f).strip().split(',')
+print(headers)
+
+sum=0
+for line in f:
+    row=line.split(',')
+    sum=sum+int(row[1])*float(row[2])
+
+
+
+print('Total Cost', sum)
+f.close()
+
