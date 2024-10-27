b59a1db2114c086ad545aad9d3e5508230585470
dedzr <salmancse.i77@gmail.com>
2024-10-27 11:42:03 +0600

    made pcost function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,35 @@
 # pcost.py
 #
 # Exercise 1.27
-f=open('Data/portfolio.csv', 'rt')
-headers=next(f).strip().split(',')
-print(headers)
-
-sum=0
-for line in f:
-    row=line.split(',')
-    sum=sum+int(row[1])*float(row[2])
+import csv
+import sys
+def portfolio_cost(filename):
 
 
+    f=open(filename, 'rt')
+    rows=csv.reader(f)
+    headers=next(rows)
+    print(headers)
 
-print('Total Cost', sum)
-f.close()
+
+    sum=0
+    for line in rows:
+       
+        try:
+            sum=sum+int(line[1])*float(line[2])
+        except ValueError:
+            print("cannot add value")
+
+
+
+    print('Total Cost', sum)
+    f.close()
+
+
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+else:
+    filename='Data/missing.csv'
+
+print(filename)
+portfolio_cost(filename)
